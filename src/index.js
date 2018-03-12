80d4f46652ea13c754071bf20f266e89b6291925
ustccjw <317713370@qq.com>
2018-03-13 00:32:22 +0800

    style: lint

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import produce from 'immer'
 
 class ImmutableComponent extends PureComponent {
   setState(updater, cb) {
-    if (typeof updater === 'function') {
-      return super.setState(updater, cb)
-    }
+    if (updater === null || typeof updater === 'function') return super.setState(updater, cb)
+
     const stateChange = updater
     const prevState = this.state || {}
     const nextState = produce(prevState, state => {
       Object.keys(stateChange).forEach(key => {
         const keyArr = key.split('.')
         const lastKey = keyArr.pop()
-        const lastState = keyArr.reduce((state2, key2) => state2[key2], state)
+        const lastState = keyArr.reduce((middleState, middleKey) => middleState[middleKey], state)
         lastState[lastKey] = stateChange[key]
       })
     })
