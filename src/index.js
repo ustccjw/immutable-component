1c9cc75b6fd74979fafdb3ae7dd26bdf9a378c9c
ustccjw <ustccjw@gmail.com>
2018-04-12 19:42:35 +0800

    fix: setState override bug

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ class ImmutableComponent extends PureComponent {
   setState(updater, cb) {
     if (updater === null || typeof updater === 'function') return super.setState(updater, cb)
 
-    const stateChange = updater
-    const prevState = this.state || {}
-    const nextState = produce(prevState, state => {
-      Object.keys(stateChange).forEach(key => {
+    this.batchState = this.batchState || this.state || {}
+    const newState = updater
+    this.batchState = produce(this.batchState, state => {
+      Object.keys(newState).forEach(key => {
         const keyArr = key.split('.')
         const lastKey = keyArr.pop()
         const lastState = keyArr.reduce((middleState, middleKey) => middleState[middleKey], state)
-        lastState[lastKey] = stateChange[key]
+        lastState[lastKey] = newState[key]
       })
     })
-    return super.setState(nextState, cb)
+    return super.setState(this.batchState, cb)
   }
 }
 
